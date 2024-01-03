c64c1405685ab431618b2b408845db9958422997
Ike <dylanraspino264@gmail.com>
2024-01-03 22:42:32 +0000

    added default key

--- s_rooms/s_rooms/settings.py
+++ s_rooms/s_rooms/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default="djangomy_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
