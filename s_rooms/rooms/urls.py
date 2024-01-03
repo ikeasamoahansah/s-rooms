d2c5105b62064d7741254ac0fc6239a599a291dd
Ike <dylanraspino264@gmail.com>
2024-01-03 23:57:31 +0000

    updated urls.py for simple view

--- s_rooms/rooms/urls.py
+++ s_rooms/rooms/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from rest_framework.generics import ListAPIView
 
+from .models import *
+from .serializers import *
 from .views import *
 
 urlpatterns = [
     path("", api_root),
-    path("home/", RoomView.as_view(), name="room-view"),
+    path(
+        "home/",
+        ListAPIView.as_view(
+            queryset=Room.objects.all(), serializer_class=RoomSerializer
+        ),
+        name="room-view",
+    ),
 ]
--- s_rooms/rooms/views.py
+++ s_rooms/rooms/views.py
@@ -1,8 +1,6 @@
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.views import APIView
 
 from .models import *
 from .serializers import *
@@ -17,20 +15,5 @@ def api_root(request, format=None):
     )
 
 
-class RoomView(APIView):
-    """
-    List all rooms
-    """
-
-    def get(self, request, format=None):
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+   
