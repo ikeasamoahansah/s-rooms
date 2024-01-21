f26a4d3952f08686babf4dfe9ee9dbe79b0c2c95
Ike <dylanraspino264@gmail.com>
2024-01-21 19:03:18 +0000

    changed serializers and models

--- s_rooms/rooms/models.py
+++ s_rooms/rooms/models.py
@@ -1,13 +1,25 @@
 import uuid
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rooms")
     name = models.CharField(max_length=256)
-    description = models.TextField()
+    current_users = models.ManyToManyField(User, related_name="current_rooms", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} | {self.host}"
+
+
+class Message(models.Model):
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE, related_name="messages")
+    text = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message({self.user} | {self.room})"
--- s_rooms/rooms/serializers.py
+++ s_rooms/rooms/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from .models import *
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ["password"]
+
+
+class MessageSerialzier(serializers.ModelSerializer):
+    created_at_formatted = serializers.SerializerMethodField()
+    user = UserSerializer()
+
+    class Meta:
+        model = Message
+        exlude = []
+        depth = 1
+
+    def get_created_at_formatted(self, obj:Message):
+        return obj.created_at.strftime("%d-%m-%Y %H:%M:%S")
+
+
 class RoomSerializer(serializers.ModelSerializer):
+    last_message = serializers.SerializerMethodField()
+    messages = MessageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Room
-        fields = ["id", "name", "description"]
+        fields = ["id", "name", "host"]
+        depth = 1
+        read_only_fields = ["messages", "last_message"]
+
+    def get_last_message(self, obj:Room):
+        return MessageSerializer(obj.messages.order_by('created_at').last()).data
+
+
