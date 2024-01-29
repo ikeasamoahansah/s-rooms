fefd8a2aa023466dd776bb0943123d19998376de
Ike <dylanraspino264@gmail.com>
2024-01-29 23:44:06 +0000

    solved password as name issue

--- s_rooms/accounts/models.py
+++ s_rooms/accounts/models.py
@@ -8,7 +8,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, name=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, name=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
@@ -19,17 +19,17 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name=None, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, name=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, name=name, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    email = models.EmailField(unique=True)
     name = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
