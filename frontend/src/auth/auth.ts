be107364f455e31e8fb9398c3791f2bf7dc0245b
Ike <dylanraspino264@gmail.com>
2024-02-20 02:16:36 +0000

    auth added for tokens

---
+++ frontend/src/auth/auth.ts
@@ -0,0 +1,42 @@
+import axios, {AxiosResponse} from 'axios';
+
+interface LoginResponse {
+    token: string;
+}
+
+export async function loginUser(username: string, password: string): Promise<string | undefined> {
+    try {
+        const response: AxiosResponse<LoginResponse> = await axios.post(
+            'http://127.0.0.1:8000/token-auth/',
+            {
+                username,
+                password
+            }
+        );
+        const {token} = response.data;
+        // Store token in local storage
+        localStorage.setItem('token', token);
+        return token;
+    } catch (error) {
+        console.error('Error logging in', error);
+        return undefined;
+    }
+}
+
+export async function refreshToken(): Promise<string | undefined> {
+    try {
+        const token = localStorage.getItem('token');
+        if (!token) throw new Error('No token stored');
+        const response: AxiosResponse<LoginResponse> = await axios.post(
+            'http://127.0.0.1/token-refresh/',
+            {token}
+        );
+        const newToken = response.data.token;
+        // Store token in local storage
+        localStorage.setItem('token', newToken);
+        return newToken;
+    }catch(error){
+        console.error('Error refreshing token', error);
+        return undefined;
+    }
+}
