1f4d156db5e03f8bfbcc969a2380c752977be5f7
Ike <dylanraspino264@gmail.com>
2024-03-07 19:58:31 +0000

    logs

--- frontend/src/pages/RoomDetail.tsx
+++ frontend/src/pages/RoomDetail.tsx
@@ -18,7 +18,9 @@ function RoomDetail(){
                     headers: {
                         Authorization: `token ${token}`
                     }
-                });                
+                });
+                console.log(response.data);
+                                
                 setRoom(response.data);
             } catch (error) {
                 console.error(error);
