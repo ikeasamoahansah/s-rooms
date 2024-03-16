fe3169f62aa959559774594387ff25ab3322c959
Ike <dylanraspino264@gmail.com>
2024-03-16 21:32:17 +0000

    new border

--- frontend/src/components/rooms/ChatInterface.tsx
+++ frontend/src/components/rooms/ChatInterface.tsx
@@ -71,7 +71,7 @@ class ChatInterface extends React.Component<ChatInterfaceProps, ChatState> {
                 </div>
                 <div className="flex items-center p-4 ">
                     <input type="text" value={newMessage} onChange={this.handleInputChange} />
-                    <button onClick={this.sendMessage} className="bg-blue-500 text-white p-3 rounded-r-lg ml-2">Send</button>
+                    <button onClick={this.sendMessage} className="bg-blue-500 text-white p-3 rounded-r-sm ml-2">Send</button>
                 </div>
             </div>
         );
