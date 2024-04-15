a7e614cbc903c478a42049aba4e38ba0b6055872
Ike <dylanraspino264@gmail.com>
2024-04-15 22:51:13 +0000

    increase access token lifetime

--- s_rooms/s_rooms/settings.py
+++ s_rooms/s_rooms/settings.py
@@ -120,8 +120,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # rest framework settings
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
