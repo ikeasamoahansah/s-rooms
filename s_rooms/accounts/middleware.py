804665a82f93e917bf98d4fb65a3e37408188b5c
Ike <dylanraspino264@gmail.com>
2024-04-16 21:16:38 +0000

    added token expiration middleware

---
+++ s_rooms/accounts/middleware.py
@@ -0,0 +1,23 @@
+from typing import Any
+from django.shortcuts import redirect
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+class TokenExpirationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request) -> Any:
+        user_auth = JWTAuthentication()
+        try:
+            user, _ = user_auth.authenticate(request)
+            if user is not None and user.is_authenticated:
+                request.user = user
+            else:
+                request.user = None
+        except (InvalidToken, TokenError):
+            if request.user.is_authenticated:
+                redirect("logout")
+
+        response = self.get_response(request)
+        return response
--- s_rooms/s_rooms/settings.py
+++ s_rooms/s_rooms/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "accounts.middleware.TokenExpirationMiddleware",
 ]
 
 ROOT_URLCONF = "s_rooms.urls"
