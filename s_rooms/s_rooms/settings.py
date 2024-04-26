493a8aba75c5962dba901cfc2f75b8ddfabe704d
Ike <dylanraspino264@gmail.com>
2024-04-26 23:44:39 +0000

    changed middleware call position

--- s_rooms/s_rooms/settings.py
+++ s_rooms/s_rooms/settings.py
@@ -47,13 +47,13 @@ REST_FRAMEWORK = {
 # AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
-    "accounts.middleware.TokenExpirationMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "accounts.middleware.TokenExpirationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
